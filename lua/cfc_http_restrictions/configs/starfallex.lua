8f12ceb276227b86581c6c740b6a7553de65a9d7
pierce <pierce@vulpes.dev>
2025-01-08 12:10:46 -0500

    disable starfall whitelist

--- lua/cfc_http_restrictions/configs/starfallex.lua
+++ lua/cfc_http_restrictions/configs/starfallex.lua
@@ -10,4 +10,27 @@ local config = {
     }
 }
 
+if CLIENT then
+    local P = {}
+    P.id = "urlwhitelist"
+    P.name = "URL Whitelist"
+    P.settingsoptions = { "Enabled", "Disabled" }
+    P.defaultsetting = 1
+    P.checks = {
+        function() return true end,
+        "allow",
+    }
+
+    P.settingsoptions[3] = "Disabled for owner"
+    P.checks[3] = function() return true end
+
+    hook.Add( "PostGamemodeLoaded", "CFCHTTP_Wrap_SF", function()
+        local original = SF.Permissions.includePermissions
+        SF.Permissions.includePermissions = function()
+            original()
+            SF.Permissions.registerProvider( P )
+        end
+    end )
+end
+
 return config
