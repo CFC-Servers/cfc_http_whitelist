5981358a3b83bfa155a77d1bdb6de10b0154ab5d
pierce <pierce@vulpes.dev>
2026-01-14 20:54:13 -0500

    include http in code

--- lua/includes/modules/http.lua
+++ lua/includes/modules/http.lua
@@ -1,11 +1,19 @@
 AddCSLuaFile()
 
--- file.Read is not available yet
-local originalFile = file.Open( "garrysmod/lua/includes/modules/http.lua", "r", "BASE_PATH" )
-local code = originalFile:Read( originalFile:Size() )
-originalFile:Close()
 
-local f = CompileString( code, "lua/includes/modules/http.lua", false )
+do
+    local expectedHash = "0a60adfc0d1336d34928479820a0e071cdfc31f95053ebf8d1b61b07f28e0f32"
+
+    -- file.Read is not available yet
+    local originalFile = file.Open( "garrysmod/lua/includes/modules/http.lua", "r", "BASE_PATH" )
+    local code = originalFile:Read( originalFile:Size() )
+    originalFile:Close()
+    local hash = util.SHA256( code )
+    if hash ~= expectedHash then
+        ErrorNoHaltWithStack( "[CFC HTTP Restrictions] Warning: The http.lua module has been modified! Expected hash: " .. expectedHash .. ", got: " .. hash .. "\n" )
+    end
+end
+
 
 if SERVER then
     local svEnabled = CreateConVar( "cfc_http_restrictions_sv_enabled", "0", FCVAR_ARCHIVE, "Enable server-side HTTP whitelisting", 0, 1 )
@@ -19,4 +27,4 @@ if CLIENT then
     ProtectedCall( include, "cfc_http_restrictions/wraps/playURL.lua" )
 end
 
-f()
+require( "original_http" )
