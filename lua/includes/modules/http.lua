876bfef0d368090982660bf5200c37d3b64031c4
pierce <pierce@vulpes.dev>
2026-03-08 18:32:09 -0400

    Revert "Add stub to top of file instead"
    This reverts commit a87fde808a9343a0f9ddff69d5f31319b847f2f8.

--- lua/includes/modules/http.lua
+++ lua/includes/modules/http.lua
@@ -1,3 +1,20 @@
+AddCSLuaFile()
+
+
+do
+    local expectedHash = "0a60adfc0d1336d34928479820a0e071cdfc31f95053ebf8d1b61b07f28e0f32"
+
+    -- file.Read is not available yet
+    local originalFile = file.Open( "garrysmod/lua/includes/modules/http.lua", "r", "BASE_PATH" )
+    local code = originalFile:Read( originalFile:Size() )
+    originalFile:Close()
+    local hash = util.SHA256( code )
+    if hash ~= expectedHash then
+        ErrorNoHaltWithStack( "[CFC HTTP Restrictions] Warning: The http.lua module has been modified! Expected hash: " .. expectedHash .. ", got: " .. hash .. "\n" )
+    end
+end
+
+
 if SERVER then
     local svEnabled = CreateConVar( "cfc_http_restrictions_sv_enabled", "0", FCVAR_ARCHIVE, "Enable server-side HTTP whitelisting", 0, 1 )
     if svEnabled:GetBool() then
@@ -10,109 +27,4 @@ if CLIENT then
     ProtectedCall( include, "cfc_http_restrictions/wraps/playURL.lua" )
 end
 
-local HTTP = HTTP
-
---[[---------------------------------------------------------
-	HTTP Module. Interaction with HTTP.
------------------------------------------------------------]]
-module( "http" )
-
---[[---------------------------------------------------------
-
-	Get the contents of a webpage.
-
-	Callback should be
-
-	function callback( (args optional), contents, size )
-
------------------------------------------------------------]]
-function Fetch( url, onsuccess, onfailure, header )
-
-	local request = {
-		url			= url,
-		method		= "get",
-		headers		= header or {},
-
-		success = function( code, body, headers )
-
-			if ( !onsuccess ) then return end
-
-			onsuccess( body, body:len(), headers, code )
-
-		end,
-
-		failed = function( err )
-
-			if ( !onfailure ) then return end
-
-			onfailure( err )
-
-		end
-	}
-
-	local success = HTTP( request )
-	if ( !success && onfailure ) then onfailure( "HTTP failed" ) end
-
-end
-
-function Post( url, params, onsuccess, onfailure, header )
-
-	local request = {
-		url			= url,
-		method		= "post",
-		parameters	= params,
-		headers		= header or {},
-
-		success = function( code, body, headers )
-
-			if ( !onsuccess ) then return end
-
-			onsuccess( body, body:len(), headers, code )
-
-		end,
-
-		failed = function( err )
-
-			if ( !onfailure ) then return end
-
-			onfailure( err )
-
-		end
-	}
-
-	local success = HTTP( request )
-	if ( !success && onfailure ) then onfailure( "HTTP failed" ) end
-
-end
-
---[[
-
-Or use HTTP( table )
-
-local request = {
-	url			= "http://pastebin.com/raw.php?i=3jsf50nL",
-	method		= "post",
-
-	parameters = {
-		id			= "548",
-		country		= "England"
-	}
-
-	success = function( code, body, headers )
-
-		Msg( "Request Successful\n" )
-		Msg( "Code: ", code, "\n" )
-		Msg( "Body Length:\n", body:len(), "\n" )
-		Msg( "Body:\n", body, "\n" )
-		PrintTable( headers )
-
-	end,
-
-	failed = function( reason )
-		Msg( "Request failed: ", reason, "\n" )
-	end
-}
-
-HTTP( request )
-
---]]
+require( "original_http" )
