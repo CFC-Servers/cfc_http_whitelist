0bee8f4b847805919d00207316927993964fcaa8
Redox <69946827+wrefgtzweve@users.noreply.github.com>
2026-07-07 23:59:57 +0200

    Add blocked log to chat messages with urls

--- lua/cfc_http_restrictions/server/chatfilter.lua
+++ lua/cfc_http_restrictions/server/chatfilter.lua
@@ -6,15 +6,26 @@ hook.Add( "PlayerSay", "CFC_HTTP_ChatFilter", function( _, text )
 
     local foundBlocked = false
     local urls = CFCHTTP.FindURLs( text )
+    local blockedURLs = {}
     for _, url in ipairs( urls ) do
         local options = CFCHTTP.GetOptionsForURL( url )
         if options.allowed == false then
             text = string.gsub( text, string.PatternSafe( url ), "BLOCKED URL" )
             foundBlocked = true
+            table.insert( blockedURLs, {
+                url = url,
+                status = "BLOCKED",
+                reason = "Blocked URL in chat message"
+            } )
         end
     end
 
     if foundBlocked then
+        CFCHTTP.LogRequest( {
+            method = "CHAT",
+            urls = blockedURLs,
+            fileLocation = debug.getinfo( 1, "S" ).short_src
+        } )
         return text
     end
 end )
