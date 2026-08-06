3d6e26cec808116868966ced20aee3de6c8c16a9
Anquietas System <76923632+anquietas-sys@users.noreply.github.com>
2026-08-05 19:24:42 -0500

    Update media player URL (#84)
    They changed the domain (again)
    This is the one they're using now.

--- lua/cfc_http_restrictions/configs/media_player.lua
+++ lua/cfc_http_restrictions/configs/media_player.lua
@@ -5,7 +5,7 @@ AddCSLuaFile()
 local config = {
     version = "1",
     addresses = {
-        ["samuelmaddock.github.io"] = { allowed = true },
+        ["mp.physcannon.top"] = { allowed = true },
     }
 }
 
