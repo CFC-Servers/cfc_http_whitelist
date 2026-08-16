748c2fe499d683943947c0a6309e6efd8a990190
Redox <69946827+wrefgtzweve@users.noreply.github.com>
2026-08-16 18:33:06 +0200

    Normalize line endings and update hash
    Fixes windows vs linux hash being different

--- lua/includes/modules/http.lua
+++ lua/includes/modules/http.lua
@@ -1,12 +1,14 @@
 AddCSLuaFile()
 
 if SERVER then
-    local expectedHash = "0a60adfc0d1336d34928479820a0e071cdfc31f95053ebf8d1b61b07f28e0f32"
+    local expectedHash = "0cd6bc47b235a6c98af53c41e575f06fbb00a1bd399f49ddcfd15f139f9a4def"
 
     -- file.Read is not available yet
     local originalFile = file.Open( "garrysmod/lua/includes/modules/http.lua", "r", "BASE_PATH" )
     local code = originalFile:Read( originalFile:Size() )
     originalFile:Close()
+
+    code = string.gsub( code, "\r\n", "\n" ) -- Normalize line endings to avoid false positives on Windows vs Linux/Mac
     local hash = util.SHA256( code )
     if hash ~= expectedHash then
         ErrorNoHaltWithStack( "[CFC HTTP Restrictions] Warning: The http.lua module has been modified! Expected hash: " .. expectedHash .. ", got: " .. hash .. "\n" )
